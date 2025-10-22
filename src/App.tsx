4dfb8b4a9d3231db8c64c9199873771b511cd218
Mas-Pey <konamexxxpes@gmail.com>
2025-10-22 22:42:32 +0700

    first time~~~

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return(
+    <main className="py-10 bg-green-900 not-dark:bg-green-200 h-screen">
+      <h1 className="font-extrabold text-green-200 not-dark:bg-green-900 text-5xl text-center">My MustDo</h1>
+      <h3 className="font-extralight italic text-2xl text-green-200 not-dark:bg-green-900 text-center">(Not to-do, but must-do!)</h3>
+    </main>
+  )
+}
+
+export default App
