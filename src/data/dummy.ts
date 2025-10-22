430411e95f7be7ce6327ffc90888fdabebcdf695
Mas-Pey <konamexxxpes@gmail.com>
2025-10-23 00:16:09 +0700

    feat: add dummy data for MustDo tasks

---
+++ src/data/dummy.ts
@@ -0,0 +1,16 @@
+import type { MustDo } from "../types/mustdo";
+
+export const dummyData: MustDo[] = [
+    {
+        id: 1,
+        title: "Learn TypeScript",
+        description: "Understand the basics of TypeScript",
+        completed: true
+    },
+    {
+        id: 2,
+        title: "Build a React App",
+        description: "Create a simple React application using TypeScript",
+        completed: false
+    },
+]
