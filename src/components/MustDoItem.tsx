c38f05ad20af27a73711af2841e6521c46b1120c
Mas-Pey <konamexxxpes@gmail.com>
2025-10-23 12:03:29 +0700

    feat:  implement onCompletedChange prop and handle checkbox state

--- src/components/MustDoItem.tsx
+++ src/components/MustDoItem.tsx
@@ -2,13 +2,17 @@ import type { MustDo } from "../types/mustdo"
 
 interface MustDoItemProps {
     mustdo: MustDo
+    onCompletedChange: (id: number, completed: boolean) => void
 }
 
-export default function MustDoItem({ mustdo }: MustDoItemProps) {
+export default function MustDoItem({ mustdo, onCompletedChange }: MustDoItemProps) {
     return (
         <div>
-            <label className="flex items-center gap-5 border rounded-3xl p-4 text-amber-200 hover:bg-green-950 not-dark:hover:bg-green-300">
-                <input type="checkbox" className="scale-150"></input>
+            <label className="flex items-center gap-5 border rounded-2xl p-4 text-amber-200 hover:bg-green-950 not-dark:hover:bg-green-300">
+                <input type="checkbox"
+                    checked={mustdo.completed}
+                    onChange={(e) => onCompletedChange(mustdo.id, e.target.checked)}
+                    className="scale-150"></input>
                 <span className={mustdo.completed ? "line-through opacity-50" : ""}>
                     {mustdo.title}
                 </span>
