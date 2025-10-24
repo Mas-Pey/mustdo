8d80312340dc3c42abb611d099fc746fe326cd01
Mas-Pey <konamexxxpes@gmail.com>
2025-10-25 00:58:20 +0700

    feat: add delete function

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { useState } from "react"
-import MustDoItem from "./components/MustDoItem"
 import { dummyData } from "./data/dummy"
 import AddMustDoForm from "./components/AddMustDoForm"
+import MustDoList from "./components/MustDoList"
 
 function App() {
   const [mustdo, setMustDo] = useState(dummyData)
 
   function setMustdoCompleted(id: number, completed: boolean) {
     setMustDo((prevMustDo) =>
-      prevMustDo.map((list) => (list.id === id ? {...list, completed} : list))
+      prevMustDo.map(list => list.id === id ? { ...list, completed } : list)
     )
   }
 
-  function addMustDo(title:string) {
+  function addMustDo(title: string) {
     setMustDo((prevMustDo) => [
       {
         id: prevMustDo.length + 1,
@@ -20,24 +20,21 @@ function App() {
         completed: false
       },
       ...prevMustDo
-    ]
+    ])
+  }
 
-    )
+  function deleteMustDo(id: number) {
+    setMustDo((prevMustDo) =>
+      prevMustDo.filter(list => list.id !== id))
   }
 
   return (
-    <main className="py-10 space-y-0.5 bg-green-900 not-dark:bg-green-200 h-screen">
+    <main className="py-10 space-y-0.5 bg-green-900 not-dark:bg-green-200 h-screen overflow-y-auto">
       <h1 className="font-extrabold text-green-200 not-dark:bg-green-900 text-5xl text-center">My MustDo</h1>
       <h3 className="font-extralight italic text-2xl text-green-200 not-dark:bg-green-900 text-center pb-5">(Not to-do, but must-do!)</h3>
-      <div className="max-w-lg mx-auto p-4 space-y-6 rounded-md bg-green-800 not-dark:bg-green-100 border-b-3 border-r-4 border-black">
-        <AddMustDoForm 
-        onSubmit={addMustDo}
-        />
-        <div className="space-y-3">
-          {mustdo.map(list => (
-            <MustDoItem mustdo={list} key={list.id} onCompletedChange={setMustdoCompleted}/>
-          ))}
-        </div>
+      <div className="max-w-lg mx-auto overflow-hidden p-4 space-y-6 rounded-md bg-green-800 not-dark:bg-green-100 border-b-3 border-r-4 border-black">
+        <AddMustDoForm onSubmit={addMustDo} />
+        <MustDoList mustdo={mustdo} onCompletedChange={setMustdoCompleted} onDelete={deleteMustDo} />
       </div>
     </main>
   )
