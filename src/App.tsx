1a053990d2cadefb7a09ca0829c16b82fbead8f6
Mas-Pey <konamexxxpes@gmail.com>
2025-10-27 19:10:17 +0700

    feat: add a function and callback for delete all items

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { dummyData } from "./data/dummy"
 import AddMustDoForm from "./components/AddMustDoForm"
 import MustDoList from "./components/MustDoList"
+import MustDoSummary from "./components/MustDoSummary"
 
 function App() {
   const [mustdo, setMustDo] = useState(dummyData)
@@ -15,7 +16,7 @@ function App() {
   function addMustDo(title: string) {
     setMustDo((prevMustDo) => [
       {
-        id: prevMustDo.length + 1,
+        id: Date.now(),
         title,
         completed: false
       },
@@ -28,10 +29,16 @@ function App() {
       prevMustDo.filter(list => list.id !== id))
   }
 
+  function deleteAllMustDo() {
+    setMustDo((prevMustDo) => 
+      prevMustDo.filter(list => !list.completed))
+  }
+
   return (
     <main className="py-10 space-y-0.5 bg-green-900 not-dark:bg-green-200 h-screen overflow-y-auto">
       <h1 className="font-extrabold text-green-200 not-dark:bg-green-900 text-5xl text-center">My MustDo</h1>
-      <h3 className="font-extralight italic text-2xl text-green-200 not-dark:bg-green-900 text-center pb-5">(Not to-do, but must-do!)</h3>
+      <h3 className="font-extralight italic text-2xl text-green-200 not-dark:bg-green-900 text-center pb-4">(Not to-do, but must-do!)</h3>
+      <MustDoSummary mustdo={mustdo} deleteAllCompleted={deleteAllMustDo} />
       <div className="max-w-lg mx-auto overflow-hidden p-4 space-y-6 rounded-md bg-green-800 not-dark:bg-green-100 border-b-3 border-r-4 border-black">
         <AddMustDoForm onSubmit={addMustDo} />
         <MustDoList mustdo={mustdo} onCompletedChange={setMustdoCompleted} onDelete={deleteMustDo} />
